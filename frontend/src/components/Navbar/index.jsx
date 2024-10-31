b4cdf140e386c40e4c5d2c3ff8aac18fa81dc0ae
Kyle Winch <KyleSWinch@outlook.com>
2024-10-30 22:14:36 -0400

    Further updated home pages styling - cleaning up info cards

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -3,19 +3,30 @@ import { Link } from 'react-router-dom';
 import './index.css';
 
 const Navbar = () => {
-    return (
-        <div>
-            <nav className="navbar">
-                <div className="navbar-logo">Kings Hockey</div>
-                <ul className="navbar-links">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/schedule">Schedule</Link></li>
-                    <li><Link to="/gallery">Gallery</Link></li>
-                </ul>
-            </nav>
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <img src="/images/crown.png" alt="Crown Logo" className="navbar-logo-icon" />
+          Kings Hockey
         </div>
-    );
+        <ul className="navbar-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/schedule">Schedule</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
--- frontend/src/routes/Home/index.jsx
+++ frontend/src/routes/Home/index.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import './index.css';
+import InfoBox from './../../components/InfoBox';
 
 const Home = () => {
   return (
     <div className="home-page">
       <div className="header-section">
         <h1>King's Hockey Team</h1>
-        <p>Winter 2024-2025 Division 8</p>
+        <h4>Winter 2024 - 2025</h4>
+        <h4>Division 8</h4>
       </div>
 
       <div className="content-section">
-        <div className="info-box">
-          <h2>About Us</h2>
-          <p>Some information about the website or the brand.</p>
-        </div>
-        <div className="info-box">
-          <h2></h2>
-          <p>Details about services or offerings.</p>
-        </div>
-        <div className="info-box">
-          <h2></h2>
-          <p></p>
-        </div>
+        <InfoBox
+          title="Last Game Recap"
+          description="Previous game information in here! Spoiler.. We suck!"
+          imageUrl="/images/rink.jpg"
+        />
+
+        <InfoBox
+          title="Temp"
+          description="Details about services or offerings."
+          imageUrl="/images/rink.jpg"
+        />
+
+        <InfoBox
+          title="Temp"
+          description="Temp add data for testing 010232"
+          imageUrl="/images/rink.jpg"
+        />
       </div>
     </div>
   );
