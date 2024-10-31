5a58b85652674b94fe31debcd4396d402a5d74a8
Kyle Winch <KyleSWinch@outlook.com>
2024-10-30 23:01:02 -0400

    Created styling for the home page (#3)
    * Updated styling for home page and added dark mode support
    
    * Further updated home pages styling - cleaning up info cards

--- frontend/src/routes/Home/index.jsx
+++ frontend/src/routes/Home/index.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
+import InfoBox from './../../components/InfoBox';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-page">
+      <div className="header-section">
+        <h1>King's Hockey Team</h1>
+        <h4>Winter 2024 - 2025</h4>
+        <h4>Division 8</h4>
+      </div>
 
-export default Home
+      <div className="content-section">
+        <InfoBox
+          title="Last Game Recap"
+          description="Previous game information in here! Spoiler.. We suck!"
+          imageUrl="/images/rink.jpg"
+        />
+
+        <InfoBox
+          title="Temp"
+          description="Details about services or offerings."
+          imageUrl="/images/rink.jpg"
+        />
+
+        <InfoBox
+          title="Temp"
+          description="Temp add data for testing 010232"
+          imageUrl="/images/rink.jpg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
