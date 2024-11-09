4a23aa1f4da24c05e267a744a4f3a23afec2ed20
Kyle Winch <KyleSWinch@outlook.com>
2024-11-09 00:20:57 -0500

    Attempting Fix for iphones (#19)

--- frontend/src/routes/Stats/index.jsx
+++ frontend/src/routes/Stats/index.jsx
@@ -50,67 +50,73 @@ const Stats = () => {
   });
 
   return (
-    <div className="table-container">
-      {error ? (
-        <p className="error-message">{error}</p>
-      ) : (
-        <div className="table-responsive-wrapper">
-          <table className="player-stats-table">
-            <thead>
-              <tr>
-                <th className="col-number">#</th>
-                <th onClick={() => requestSort('name')}>Name</th>
-                <th className="col-gp" onClick={() => requestSort('games_played')}>
-                  GP
-                </th>
-                <th className="col-goals" onClick={() => requestSort('goals')}>
-                  G
-                </th>
-                <th className="col-assists" onClick={() => requestSort('assists')}>
-                  A
-                </th>
-                <th className="col-points" onClick={() => requestSort('points')}>
-                  Pts
-                </th>
-                {/* Other columns hidden on mobile */}
-                <th className="col-ppga" onClick={() => requestSort('points_per_game')}>
-                  PPGA
-                </th>
-                <th className="col-pim" onClick={() => requestSort('penalty_minutes')}>
-                  PIM
-                </th>
-                <th className="col-ppg" onClick={() => requestSort('power_play_goals')}>
-                  PPG
-                </th>
-                <th className="col-shg" onClick={() => requestSort('short_handed_goals')}>
-                  SHG
-                </th>
-                <th className="col-gwg" onClick={() => requestSort('game_winning_goals')}>
-                  GWG
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {sortedPlayerStats.map((player, index) => (
-                <tr key={index} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
-                  <td className="col-number">{player.jersey_number}</td>
-                  <td>{player.name}</td>
-                  <td className="col-gp">{player.games_played}</td>
-                  <td className="col-goals">{player.goals}</td>
-                  <td className="col-assists">{player.assists}</td>
-                  <td className="col-points">{player.points}</td>
-                  <td className="col-ppga">{player.points_per_game}</td>
-                  <td className="col-pim">{player.penalty_minutes}</td>
-                  <td className="col-ppg">{player.power_play_goals}</td>
-                  <td className="col-shg">{player.short_handed_goals}</td>
-                  <td className="col-gwg">{player.game_winning_goals}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    <>
+      {playerStats ? (
+        <div className="table-container">
+          {error ? (
+            <p className="error-message">{error}</p>
+          ) : (
+            <div className="table-responsive-wrapper">
+              <table className="player-stats-table">
+                <thead>
+                  <tr>
+                    <th className="col-number">#</th>
+                    <th onClick={() => requestSort('name')}>Name</th>
+                    <th className="col-gp" onClick={() => requestSort('games_played')}>
+                      GP
+                    </th>
+                    <th className="col-goals" onClick={() => requestSort('goals')}>
+                      G
+                    </th>
+                    <th className="col-assists" onClick={() => requestSort('assists')}>
+                      A
+                    </th>
+                    <th className="col-points" onClick={() => requestSort('points')}>
+                      Pts
+                    </th>
+                    {/* Other columns hidden on mobile */}
+                    <th className="col-ppga" onClick={() => requestSort('points_per_game')}>
+                      PPGA
+                    </th>
+                    <th className="col-pim" onClick={() => requestSort('penalty_minutes')}>
+                      PIM
+                    </th>
+                    <th className="col-ppg" onClick={() => requestSort('power_play_goals')}>
+                      PPG
+                    </th>
+                    <th className="col-shg" onClick={() => requestSort('short_handed_goals')}>
+                      SHG
+                    </th>
+                    <th className="col-gwg" onClick={() => requestSort('game_winning_goals')}>
+                      GWG
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sortedPlayerStats.map((player, index) => (
+                    <tr key={index} className={index % 2 === 0 ? 'even-row' : 'odd-row'}>
+                      <td className="col-number">{player.jersey_number}</td>
+                      <td>{player.name}</td>
+                      <td className="col-gp">{player.games_played}</td>
+                      <td className="col-goals">{player.goals}</td>
+                      <td className="col-assists">{player.assists}</td>
+                      <td className="col-points">{player.points}</td>
+                      <td className="col-ppga">{player.points_per_game}</td>
+                      <td className="col-pim">{player.penalty_minutes}</td>
+                      <td className="col-ppg">{player.power_play_goals}</td>
+                      <td className="col-shg">{player.short_handed_goals}</td>
+                      <td className="col-gwg">{player.game_winning_goals}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      ) : (
+        <span>Loading</span>
+      )}{' '}
+    </>
   );
 };
 
