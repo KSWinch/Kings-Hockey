f39c281419a55f66a0062d987eb972ef7fe343c3
Donald <77713078+Dsincennes@users.noreply.github.com>
2024-11-11 13:02:00 -0500

    Test cors (#25)
    * removed unneeded files
    
    * testing allow all cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import standingsRoutes from "./routes/standingsRoutes.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 
 app.use(express.json());
 app.use("/users", userRoutes);
