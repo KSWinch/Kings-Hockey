705b9b8bcbe00fa6bf499b37d930906afcb654d3
Gavin <tai.gavin@gmail.com>
2024-11-13 23:46:32 -0500

    added crhl logo for all other teams besides Kings

--- frontend/src/routes/Schedule/index.jsx
+++ frontend/src/routes/Schedule/index.jsx
@@ -1,17 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import './index.css';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
 const Schedule = () => {
   const [gamesData, setGamesData] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchGamesData = async () => {
@@ -28,47 +19,9 @@ const Schedule = () => {
     fetchGamesData();
   }, []);
 
-  const handleRowClick = (game) => {
-    navigate(`/schedule/attendance/${game.id}`, { state: { game } });
-  };
-
   return (
     <div className="schedule-page">
       <div className="schedule-table-container">
-        <div className="schedule-table-responsive-wrapper">
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead sx={{ backgroundColor: '#696969' }}>
-                <TableRow className="darker-row">
-                  <TableCell align="center">Home Team</TableCell>
-                  <TableCell align="center">Away Team</TableCell>
-                  <TableCell align="center">Date</TableCell>
-                  <TableCell align="center">Time</TableCell>
-                  <TableCell align="center">Location</TableCell>
-                  <TableCell align="center">Rink</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {gamesData.map((game, index) => (
-                  <TableRow
-                    key={game.id}
-                    className={index % 2 === 0 ? 'schedule-even-row' : 'schedule-odd-row'}
-                    onClick={() => handleRowClick(game)}
-                    style={{ cursor: 'pointer' }}
-                  >
-                    <TableCell align="center">{game.home_team}</TableCell>
-                    <TableCell align="center">{game.away_team}</TableCell>
-                    <TableCell align="center">{game.date}</TableCell>
-                    <TableCell align="center">{game.time}</TableCell>
-                    <TableCell align="center">{game.location}</TableCell>
-                    <TableCell align="center">{game.rink}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
-
         <div className="game-cards-container">
           {gamesData.map((game, index) => (
             <div className={`game-card ${index % 2 === 0 ? 'even-row' : 'odd-row'}`} key={game.id}>
@@ -77,13 +30,21 @@ const Schedule = () => {
               </div>
               <div className="teams">
                 <div className="team">
-                  <img src="/favicon.ico" alt={`${game.home_team} logo`} className="team-logo" />
+                  {game.home_team === "Kings" ? (
+                    <img src="images/crown.png" alt={`${game.home_team} logo`} className="team-logo" />
+                  ) : (
+                    <img src="images/crhl-logo.png" alt={`${game.home_team} logo`} className="team-logo" />
+                  )}
                   <div>{game.home_team}</div>
                   <div>{game.home_odds}</div>
                 </div>
                 <div>@</div>
                 <div className="team">
-                  <img src="/favicon.ico" alt={`${game.away_team} logo`} className="team-logo" />
+                  {game.away_team === "Kings" ? (
+                    <img src="images/crown.png" alt={`${game.away_team} logo`} className="team-logo" />
+                  ) : (
+                    <img src="images/crhl-logo.png" alt={`${game.away_team} logo`} className="team-logo" />
+                  )}
                   <div>{game.away_team}</div>
                   <div>{game.away_odds}</div>
                 </div>
