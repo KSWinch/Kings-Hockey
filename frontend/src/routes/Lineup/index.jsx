6f4c228d8372ca74854f2f3fef3ac367601c40c3
Donald <77713078+Dsincennes@users.noreply.github.com>
2024-11-16 01:52:23 -0500

    added gavin, fixed mobile view for lineup page (#44)

--- frontend/src/routes/Lineup/index.jsx
+++ frontend/src/routes/Lineup/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './index.module.scss';
 import PlayerCard from '../../components/PlayerCard';
 import AngusPhoto from '../../assets/images/angus_card_photo.JPEG';
+import GavinPhoto from '../../assets/images/gav-sens-photo.jpg';
 
 const Lineup = () => {
   return (
@@ -11,6 +12,30 @@ const Lineup = () => {
         <PlayerCard
           name="Angus Leung"
           photo={AngusPhoto}
+          position="LW"
+          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
+        />
+        <PlayerCard
+          name="Gavin Tai"
+          photo={GavinPhoto}
+          position="LW"
+          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
+        />
+        <PlayerCard
+          name="Gavin Tai"
+          photo={GavinPhoto}
+          position="LW"
+          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
+        />
+        <PlayerCard
+          name="Gavin Tai"
+          photo={GavinPhoto}
+          position="LW"
+          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
+        />
+        <PlayerCard
+          name="Gavin Tai"
+          photo={GavinPhoto}
           position="LW"
           teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
         />
