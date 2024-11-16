caeb8656b9a966a520b8709ea93338390bb192aa
Kyle Winch <KyleSWinch@outlook.com>
2024-11-15 20:00:00 -0500

    Co-authored-by: Donald <Dsincennes@users.noreply.github.com>
    DSincennes - Helped plan initial design
    
    KSWinch -
    Added recent game scores above Navbar
    
    Added css styling for mobile devices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from './components/Navbar';
+import ScoreHeader from './routes/ScoreHeader';
 
 function App() {
   return (
     <>
+      <ScoreHeader />
       <Navigation />
       <Outlet />
     </>
