cfc4cc45e1a7290a5dd721308f44bce152ae6a48
dsincennes <Donaldsincennes@hotmail.com>
2024-11-16 01:43:12 -0500

    added gavin, fixed mobile view for lineup page

--- frontend/src/components/PlayerCard/index.jsx
+++ frontend/src/components/PlayerCard/index.jsx
@@ -9,17 +9,10 @@ const PlayerCard = ({ name, position, photo, teamLogo }) => {
   };
 
   return (
-    <div
-      className={`${styles['player-card']} ${isFlipped ? styles['flipped'] : ''}`}
-      onClick={handleCardClick}
-    >
+    <div className={`${styles['player-card']} ${isFlipped ? styles['flipped'] : ''}`} onClick={handleCardClick}>
       <div className={styles['card-front']}>
         <div className={styles['card-name-bar']}>
-          <img
-            src={'https://www.crhl.com/wp-content/themes/f5-main/images/header-logo.png'}
-            style={{ width: '50px' }}
-            alt={`crhl_logo`}
-          />
+          <img src={'https://www.crhl.com/wp-content/themes/f5-main/images/header-logo.png'} style={{ width: '50px' }} alt={`crhl_logo`} />
           <div className={styles['card-name-container']}>
             <p className={styles['card-position']}>{position}</p>
             <h1 className={styles['card-name']}>{name}</h1>
