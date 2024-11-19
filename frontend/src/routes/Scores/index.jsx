e1d72187bf8cb927855c6da99c3dc1716c605290
Gavin <tai.gavin@gmail.com>
2024-11-18 23:36:55 -0500

    bolded the winners, faded the losers for scores (#55)
    * bolded the winners, faded the losers for scores
    
    * fixed logic
    
    ---------
    
    Co-authored-by: dsincennes <Donaldsincennes@hotmail.com>

--- frontend/src/routes/Scores/index.jsx
+++ frontend/src/routes/Scores/index.jsx
@@ -28,59 +28,29 @@ const Scores = () => {
       <div className="scores-table-container">
         <div className="scores-game-cards-container">
           <div className="scores-table-header">Scores</div>
-          {gamesData.map((game, index) => {
-            const isKingsHome = game.home_team === 'Kings';
-
-            const kingsScore = isKingsHome ? game.home_score : game.away_score;
-            const opponentScore = isKingsHome ? game.away_score : game.home_score;
-
-            const isKingsWin = kingsScore > opponentScore;
-            const resultText = isKingsWin ? 'Win' : 'Loss';
-            const resultClass = isKingsWin ? 'win' : 'loss';
-
-            return (
-              <div className={`scores-game-card ${index % 2 === 0 ? 'odd-row' : 'even-row'}`} key={game.id}>
-                <div className="game-time">{game.date}</div>
-
-                <div className="team">
-                  {isKingsHome ? (
-                    <>
-                      <img src="images/crown.png" alt="Kings logo" className="team-logo" />
-                      <span className="team-name">{game.home_team}</span>
-                      <span className="team-score">{game.home_score}</span>
-                    </>
-                  ) : (
-                    <>
-                      <img src="images/crhl-logo.png" alt={`${game.home_team} logo`} className="team-logo" />
-                      <span className="team-name">{game.home_team}</span>
-                      <span className="team-score">{game.home_score}</span>
-                    </>
-                  )}
-                </div>
-
-                <div className="team">
-                  {!isKingsHome ? (
-                    <>
-                      <img src="images/crown.png" alt="Kings logo" className="team-logo" />
-                      <span className="team-name">{game.away_team}</span>
-                      <span className="team-score">{game.away_score}</span>
-                    </>
-                  ) : (
-                    <>
-                      <img src="images/crhl-logo.png" alt={`${game.away_team} logo`} className="team-logo" />
-                      <span className="team-name">{game.away_team}</span>
-                      <span className="team-score">{game.away_score}</span>
-                    </>
-                  )}
-                </div>
-
-                {/* Display Win/Loss result at the bottom middle */}
-                <div className="game-result">
-                  <span className={`result-text ${resultClass}`}>{resultText}</span>
-                </div>
+          {gamesData.map((game, index) => (
+            <div className={`scores-game-card ${index % 2 === 0 ? 'odd-row' : 'even-row'}`} key={game.id}>
+              <div className="game-time">{game.date}</div>
+              <div className={parseInt(game.home_score) > parseInt(game.away_score) ? 'team' : 'team-loss'}>
+                {game.home_team === 'Kings' ? (
+                  <img src="images/crown.png" alt={`${game.home_team} logo`} className="team-logo" />
+                ) : (
+                  <img src="images/crhl-logo.png" alt={`${game.home_team} logo`} className="team-logo" />
+                )}
+                <span className="team-name">{game.home_team}</span>
+                <span className="team-score">{game.home_score}</span>
               </div>
-            );
-          })}
+              <div className={parseInt(game.away_score) > parseInt(game.home_score) ? 'team' : 'team-loss'}>
+                {game.away_team === 'Kings' ? (
+                  <img src="images/crown.png" alt={`${game.away_team} logo`} className="team-logo" />
+                ) : (
+                  <img src="images/crhl-logo.png" alt={`${game.away_team} logo`} className="team-logo" />
+                )}
+                <span className="team-name">{game.away_team}</span>
+                <span className="team-score">{game.away_score}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
