b6ea712bec38e223aa2469efeb7f72bd725b027e
Kyle Winch <KyleSWinch@outlook.com>
2024-11-21 00:26:37 -0500

    Top of page (#60)
    * Added scroll to top button and fixed css for mobile home page.
    
    * Solving Angus Lee Young issue for white space on game header
    
    * Updated ScrollToTop button css
    
    * Home header CSS for mobile
    
    * Cleaned up scroll button to be more efficient
    
    * Cleaned final commit
    
    * Fixed Css
    
    * add debounce
    
    ---------
    
    Co-authored-by: dsincennes <Donaldsincennes@hotmail.com>
    Co-authored-by: Donald <77713078+Dsincennes@users.noreply.github.com>

--- frontend/src/components/ScrollToTopButton/index.jsx
+++ frontend/src/components/ScrollToTopButton/index.jsx
@@ -1,33 +1,31 @@
-import React, { useState, useEffect, useRef } from 'react';
-import debounce from 'lodash/debounce';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
+
+// Custom debounce function
+const debounce = (func, wait) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+};
 
 const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  const isVisibleRef = useRef(false);
-
   useEffect(() => {
-    // Debounced function to handle scroll events
     const toggleVisibility = debounce(() => {
-      const shouldBeVisible = window.scrollY > 150;
-
-      // Update state only if it has changed
-      if (isVisibleRef.current !== shouldBeVisible) {
-        isVisibleRef.current = shouldBeVisible;
-        setIsVisible(shouldBeVisible);
+      if (window.scrollY > 150) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
       }
-    }, 100); // Debounce delay in milliseconds
+    }, 200); // Adjust the debounce delay as needed
 
-    // Add the scroll event listener
     window.addEventListener('scroll', toggleVisibility);
 
-    return () => {
-      // Remove the scroll event listener and cancel debounce
-      window.removeEventListener('scroll', toggleVisibility);
-      toggleVisibility.cancel(); // Cancel pending debounced calls
-    };
-  }, []); // Empty dependency array to set up once
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
 
   const scrollToTop = () => {
     window.scrollTo({
