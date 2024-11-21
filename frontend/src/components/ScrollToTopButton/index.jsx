4db7aeaeff34af108b81a122aacb868a953f34d8
Kyle Winch <KyleSWinch@outlook.com>
2024-11-21 00:00:19 -0500

    Fixed Css

--- frontend/src/components/ScrollToTopButton/index.jsx
+++ frontend/src/components/ScrollToTopButton/index.jsx
@@ -1,33 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react';
-import debounce from 'lodash/debounce';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
 
 const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  const isVisibleRef = useRef(false);
-
   useEffect(() => {
-    // Debounced function to handle scroll events
-    const toggleVisibility = debounce(() => {
-      const shouldBeVisible = window.scrollY > 150;
-
-      // Update state only if it has changed
-      if (isVisibleRef.current !== shouldBeVisible) {
-        isVisibleRef.current = shouldBeVisible;
-        setIsVisible(shouldBeVisible);
+    const toggleVisibility = () => {
+      if (window.scrollY > 150) {
+        //300 is more realistic but 150 so its usable on our site
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
       }
-    }, 100); // Debounce delay in milliseconds
+    };
 
-    // Add the scroll event listener
     window.addEventListener('scroll', toggleVisibility);
 
-    return () => {
-      // Remove the scroll event listener and cancel debounce
-      window.removeEventListener('scroll', toggleVisibility);
-      toggleVisibility.cancel(); // Cancel pending debounced calls
-    };
-  }, []); // Empty dependency array to set up once
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
 
   const scrollToTop = () => {
     window.scrollTo({
