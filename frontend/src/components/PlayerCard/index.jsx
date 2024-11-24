d4a34bef650c9b0ee6f70b951475f99552395150
Angus-Leung <angusleung7@gmail.com>
2024-11-24 16:58:22 -0500

    Lineup page new layout (#64)
    * lineup page modifications for mobile view and added defense and goalie lines
    
    * Remove unused var

--- frontend/src/components/PlayerCard/index.jsx
+++ frontend/src/components/PlayerCard/index.jsx
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import styles from './index.module.scss';
-
+// import { ec2ip } from '../../utils/constants';
 const PlayerCard = ({ name, position, photo, teamLogo }) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  // const [playerStats, setPlayerStats] = useState({});
+  // useEffect(() => {
+  //   const fetchPlayerStats = async () => {
+  //     try {
+  //       const response = await fetch(`${ec2ip}/stats`);
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       const data = await response.json();
+  //       setPlayerStats(data);
+  //     } catch (error) {
+  //       console.error('Error fetching player stats:', error);
+  //     }
+  //   };
+
+  //   fetchPlayerStats();
+  // }, []);
 
   const handleCardClick = () => {
     setIsFlipped(!isFlipped);
@@ -12,7 +29,11 @@ const PlayerCard = ({ name, position, photo, teamLogo }) => {
     <div className={`${styles['player-card']} ${isFlipped ? styles['flipped'] : ''}`} onClick={handleCardClick}>
       <div className={styles['card-front']}>
         <div className={styles['card-name-bar']}>
-          <img src={'https://www.crhl.com/wp-content/themes/f5-main/images/header-logo.png'} style={{ width: '50px' }} alt={`crhl_logo`} />
+          <img
+            alt={`crhl_logo`}
+            className={styles['league-logo']}
+            src={'https://www.crhl.com/wp-content/themes/f5-main/images/header-logo.png'}
+          />
           <div className={styles['card-name-container']}>
             <p className={styles['card-position']}>{position}</p>
             <h1 className={styles['card-name']}>{name}</h1>
--- frontend/src/routes/Lineup/index.jsx
+++ frontend/src/routes/Lineup/index.jsx
@@ -2,43 +2,58 @@ import React from 'react';
 import styles from './index.module.scss';
 import PlayerCard from '../../components/PlayerCard';
 import AngusPhoto from '../../assets/images/angus_card_photo.JPEG';
-import GavinPhoto from '../../assets/images/gav-sens-photo.jpg';
+// import GavinPhoto from '../../assets/images/gav-sens-photo.jpg';
 
+const stockImageUrl = 'https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png';
+const teamLogo = 'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png';
 const Lineup = () => {
   return (
     <div className={styles['lineup-page']}>
+      {/* ------------- FORWARDS ----------------- */}
+      <div className={styles['position-title']}>
+        <h1>Forwards</h1>
+      </div>
+      <div className={styles['position-subtitle']}>
+        <h2>LW</h2>
+        <h2>C</h2>
+        <h2>RW</h2>
+      </div>
       <div className={styles['card-line']}>
-        <p className={styles['title']}>WIP</p>
-        <PlayerCard
-          name="Angus Leung"
-          photo={AngusPhoto}
-          position="LW"
-          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
-        />
-        <PlayerCard
-          name="Gavin Tai"
-          photo={GavinPhoto}
-          position="LW"
-          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
-        />
-        <PlayerCard
-          name="Gavin Tai"
-          photo={GavinPhoto}
-          position="LW"
-          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
-        />
-        <PlayerCard
-          name="Gavin Tai"
-          photo={GavinPhoto}
-          position="LW"
-          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
-        />
-        <PlayerCard
-          name="Gavin Tai"
-          photo={GavinPhoto}
-          position="LW"
-          teamLogo={'https://i.pinimg.com/originals/e1/b4/cb/e1b4cb36a5f699416d31c67e5210077d.png'}
-        />
+        <PlayerCard name="Andrew Feniak" photo={stockImageUrl} position="LW" teamLogo={teamLogo} />
+        <PlayerCard name="Cody Hermann" photo={stockImageUrl} position="C" teamLogo={teamLogo} />
+        <PlayerCard name="Morille Njau" photo={stockImageUrl} position="RW" teamLogo={teamLogo} />
+      </div>
+      <hr className={styles['line-divider']} />
+      <div className={styles['card-line']}>
+        <PlayerCard name="Dom Heallis" photo={stockImageUrl} position="LW" teamLogo={teamLogo} />
+        <PlayerCard name="James Feniak" photo={stockImageUrl} position="C" teamLogo={teamLogo} />
+        <PlayerCard name="Gavin Tai" photo={stockImageUrl} position="RW" teamLogo={teamLogo} />
+      </div>
+      <hr className={styles['line-divider']} />
+      <div className={styles['card-line']}>
+        <PlayerCard name="Xiaoyu Du" photo={stockImageUrl} position="LW" teamLogo={teamLogo} />
+        <PlayerCard name="Donald Sincennes" photo={stockImageUrl} position="C" teamLogo={teamLogo} />
+        <PlayerCard name="Angus Leung" photo={AngusPhoto} position="RW" teamLogo={teamLogo} />
+      </div>
+      {/* ------------- DEFENSE ----------------- */}
+      <div className={styles['position-title']}>
+        <h1>Defensive Pairings</h1>
+      </div>
+      <div className={`${styles['card-line']} ${styles['defensive-pairings']}`}>
+        <PlayerCard name="Mike Divs" photo={stockImageUrl} position="D" teamLogo={teamLogo} />
+        <PlayerCard name="Keith Zhang" photo={stockImageUrl} position="D" teamLogo={teamLogo} />
+      </div>
+      <hr className={styles['line-divider']} />
+      <div className={`${styles['card-line']} ${styles['defensive-pairings']}`}>
+        <PlayerCard name="David Feniak" photo={stockImageUrl} position="D" teamLogo={teamLogo} />
+        <PlayerCard name="Brandon Crosby" photo={stockImageUrl} position="D" teamLogo={teamLogo} />
+      </div>
+      {/* ------------- Goalie ----------------- */}
+      <div className={styles['position-title']}>
+        <h1>Goalie</h1>
+      </div>
+      <div className={`${styles['card-line']} ${styles['goalie']}`}>
+        <PlayerCard name="Freedo Layy" photo={stockImageUrl} position="G" teamLogo={teamLogo} />
       </div>
     </div>
   );
