f0a5ab339bcee4bdc3a12a66f2852765a324cd6d
dsincennes <Donaldsincennes@hotmail.com>
2024-11-24 23:17:06 -0500

    update home page score header

--- frontend/src/routes/ScoreHeader/index.jsx
+++ frontend/src/routes/ScoreHeader/index.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import './index.css';
-import { ec2ip } from '../../utils/constants';
+import React, { useState, useEffect } from 'react';
+import './index.scss';
 
 const ScoreHeader = () => {
   const [gamesData, setGamesData] = useState([]);
@@ -8,9 +7,9 @@ const ScoreHeader = () => {
   useEffect(() => {
     const fetchGamesData = async () => {
       try {
-        const response = await fetch(`${ec2ip}/games`);
+        const response = await fetch('/api/games');
         const data = await response.json();
-        const pastGames = data.sort((a, b) => new Date(a.date + ' 2024') - new Date(b.date + ' 2024'));
+        const pastGames = data.filter((game) => new Date(game.date) < new Date());
         setGamesData(pastGames);
       } catch (error) {
         console.error('Error fetching games data:', error);
@@ -24,29 +23,34 @@ const ScoreHeader = () => {
     <div className="score-header-page">
       <div className="score-header-table-container">
         <div className="score-header-game-cards-container">
-          {gamesData.map((game, index) => (
-            <div className={`score-header-game-card ${index % 2 === 0 ? 'odd-row' : 'even-row'}`} key={game.id}>
-              <div className="game-time">{game.date}</div>
-              <div className="team">
-                {game.home_team === 'Kings' ? (
-                  <img src="images/crown.png" alt={`${game.home_team} logo`} className="team-logo" />
-                ) : (
-                  <img src="images/crhl-logo.png" alt={`${game.home_team} logo`} className="team-logo" />
-                )}
-                <span className="team-name">{game.home_team}</span>
-                <span className="team-score">{game.home_score}</span>
+          {gamesData.map((game, index) => {
+            const homeTeamLost = game.home_score < game.away_score;
+            const awayTeamLost = game.away_score < game.home_score;
+
+            return (
+              <div className={`score-header-game-card ${index % 2 === 0 ? 'odd-row' : 'even-row'}`} key={game.id}>
+                <div className="game-time">{game.date}</div>
+                <div className={`team ${homeTeamLost ? 'lost' : ''}`}>
+                  {game.home_team === 'Kings' ? (
+                    <img src="images/crown.png" alt={`${game.home_team} logo`} className="team-logo" />
+                  ) : (
+                    <img src="images/crhl-logo.png" alt={`${game.home_team} logo`} className="team-logo" />
+                  )}
+                  <span className="team-name">{game.home_team}</span>
+                  <span className="team-score">{game.home_score}</span>
+                </div>
+                <div className={`team ${awayTeamLost ? 'lost' : ''}`}>
+                  {game.away_team === 'Kings' ? (
+                    <img src="images/crown.png" alt={`${game.away_team} logo`} className="team-logo" />
+                  ) : (
+                    <img src="images/crhl-logo.png" alt={`${game.away_team} logo`} className="team-logo" />
+                  )}
+                  <span className="team-name">{game.away_team}</span>
+                  <span className="team-score">{game.away_score}</span>
+                </div>
               </div>
-              <div className="team">
-                {game.away_team === 'Kings' ? (
-                  <img src="images/crown.png" alt={`${game.away_team} logo`} className="team-logo" />
-                ) : (
-                  <img src="images/crhl-logo.png" alt={`${game.away_team} logo`} className="team-logo" />
-                )}
-                <span className="team-name">{game.away_team}</span>
-                <span className="team-score">{game.away_score}</span>
-              </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
