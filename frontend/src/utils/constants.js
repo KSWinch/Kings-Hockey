6e9785e43bd2c390d6c1dd788cc49d05565b0c25
dsincennes <Donaldsincennes@hotmail.com>
2024-11-26 21:14:36 -0500

    added nationalities

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,18 @@
 export const ec2ip = 'http://54.167.67.32:8080'; // ec2
 // export const ec2ip = 'http://localhost:8080'; // local
+
+export const countryMapping = {
+  'Dom Heallis': 'Nigeria',
+  'Keith Zhang': 'China',
+  'Donald Sincennes': 'Canada',
+  'Andrew Feniak': 'Canada',
+  'Cody Hermann': 'Canada',
+  'Brandon Crosby': 'Canada',
+  'Mo Njau': 'Tanzania',
+  'Dave Feniak': 'Canada',
+  'Angus Leung': 'HongKong',
+  'James Feniak': 'Canada',
+  'Gavin Tai': 'HongKong',
+  'Mike Divins': 'Canada',
+  'Shawn Du': 'China',
+};
